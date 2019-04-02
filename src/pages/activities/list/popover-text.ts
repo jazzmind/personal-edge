1108bad182040492c5a47f2edd1258520ee38f34
chaw <cshen_581@hotmail.com>
2019-04-02 12:48:56 +0800

    portfolio text update

--- src/pages/activities/list/popover-text.ts
+++ src/pages/activities/list/popover-text.ts
@@ -11,9 +11,9 @@ import {  } from '';
           <button col-2 (click)="closeModal()" class="close" menuClose="right">&times;</button>
         </ion-list-header>
       </ion-row>
-      <p>Your overall grade is determined by the highest 2 grades you earn for each skill.</p>
+      <p>Your overall grade is determined by the highest grades you earn for each skill.</p>
       <p>The more submissions you do for each skill, the more chance you have of earning a higher grade.</p>
-      <p>Once you are happy with your grade you can apply for that to be your final grade and be issued a “Personal Edge Digital Portfolio”.</p>
+      <p>Once you have minimum 2.5 grade you can apply for that to be your final grade and be issued a “Personal Edge Digital Portfolio”.</p>
       <p>Please note, that once you have requested the digital portfolio you grade can not be changed by doing more submissions. It will be final.</p>
     </ion-list>
   `
@@ -27,7 +27,7 @@ export class PopoverTextPage {
 
   }
   ionViewWillEnter(){
-    
+
   }
   // close popover
   closeModal() {
