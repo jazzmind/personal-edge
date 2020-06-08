61e227fbb7c10a646068f7f91d125c9fd062c33a
chaw <cshen_581@hotmail.com>
2020-06-08 14:53:47 +0800

    [PE-313] text changes

--- src/pages/activities/list/popover-text.ts
+++ src/pages/activities/list/popover-text.ts
@@ -12,7 +12,7 @@ import {  } from '';
         </ion-list-header>
       </ion-row>
       <p>Your overall grade is determined by the highest grade you earn for each skill.</p>
-      <p>Once you have at least a 2.5 grade in every skill you can apply for a “Global Skills Passport Portfolio”.</p>
+      <p>Once you have at least a 2.5 grade in every skill you can apply for a “Portfolio”.</p>
       <p>Please note, that once you have requested the portfolio you grade can not be changed by doing more submissions. It will be final.</p>
     </ion-list>
   `
